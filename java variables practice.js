a4e4dd605e6f993fe418908e748b782444f88130
Rukhsar Nasir <rukhsarnasir13@gmail.com>
2024-08-07 21:57:33 +0500

    variable learning
    Data type declaration and initialization in JavaScript.

---
+++ java variables practice.js
@@ -0,0 +1,61 @@
+fullName = "RukhsarNasir"
+console.log(fullName); 
+age = 22;
+console.log(age);
+price = 12.09
+console.log(price);
+isFollow = true
+console.log(isFollow);
+x = null;
+console.log(x);
+y = undefined;
+console.log(y);
+
+let name = "Shaista"
+name = "kalsoom";
+console.log(name);
+let a;
+console.log(a);
+
+
+var price = 25;
+
+var price = 21;
+console.log(price);
+
+const radius = 14;
+console.log(radius);
+// const b;
+// console.log(b);
+
+{
+    let price = 10;
+    console.log(price);
+}
+{
+    let price = 10;
+    console.log(price);
+    
+}
+
+let $abc = BigInt("123");
+console.log($abc);
+let abc = Symbol("Hi");
+console.log(abc);
+
+
+    const profile ={
+
+    username:"shradhakapra",
+    isFollow :true,
+    post:159,
+    following: 4,
+    followers: 569,
+    bio: "to educate someone is the heighest privilage",
+    };
+    console.log(profile);
+
+
+
+
+
